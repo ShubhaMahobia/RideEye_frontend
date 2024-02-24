91bd6ced2ea08a715e941cdaf373455d6374ecdb
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-02-24 20:35:50 +0530

    forgot password function completed

---
+++ lib/authentication/forgot_pw_email_verification.dart
@@ -0,0 +1,133 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rideeye/authentication/login_screen.dart';
+import 'package:rideeye/controllers/login_controller.dart';
+
+import 'package:rideeye/utils/buttons/b1.dart';
+import 'package:rideeye/utils/textFields/inputDecoration.dart';
+
+class ForgotPasswordEmailVerification extends StatefulWidget {
+  const ForgotPasswordEmailVerification({super.key});
+
+  @override
+  State<ForgotPasswordEmailVerification> createState() =>
+      _ForgotPasswordEmailVerificationState();
+}
+
+final LoginController _loginController = Get.put(LoginController());
+
+class _ForgotPasswordEmailVerificationState
+    extends State<ForgotPasswordEmailVerification> {
+  @override
+  void dispose() {
+    _loginController.emailController.clear();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        body: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Image.asset(
+                  'assets/icons/ride_logo_trsprnt.png',
+                  height: MediaQuery.of(context).size.height * 0.1175,
+                  width: MediaQuery.of(context).size.width * 0.533,
+                ),
+                SizedBox(
+                  height: 40,
+                  width: 320,
+                  child: Text(
+                    'Forgot Your Password?',
+                    style: GoogleFonts.plusJakartaSans(
+                      fontSize: 28,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.20,
+                ),
+                SizedBox(
+                  height: 50,
+                  width: 250,
+                  child: Text(
+                    'Enter your registered email address to reset your password',
+                    style: GoogleFonts.plusJakartaSans(
+                      fontSize: 16,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  decoration: FormInputField.formTextFieldContainer(context),
+                  child: TextField(
+                    keyboardType: TextInputType.emailAddress,
+                    controller: _loginController.emailController,
+                    textAlign: TextAlign.center,
+                    decoration:
+                        FormInputField.formTextField(context, 'Email address*'),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.25,
+                ),
+                ButtonOne(
+                    buttonText: 'Send OTP',
+                    onTap: () {
+                      _loginController.sendVerifcationMailForgotPassword(
+                          _loginController.emailController.text);
+                    }),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height * 0.05,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already Member? ',
+                      style: GoogleFonts.plusJakartaSans(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 14,
+                      ),
+                      textAlign: TextAlign.center,
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Get.to(() => const LoginScreen(),
+                            transition: Transition.noTransition);
+                      },
+                      child: Text(
+                        'Log in',
+                        style: GoogleFonts.plusJakartaSans(
+                            fontSize: 14,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/controllers/login_controller.dart
+++ lib/controllers/login_controller.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:get/get.dart';
+import 'package:rideeye/authentication/email_verification_fw.dart';
+import 'package:rideeye/authentication/login_screen.dart';
+import 'package:rideeye/authentication/reset_password.dart';
 import 'package:rideeye/home_page.dart';
 import 'package:rideeye/utils/dialogBox/error_dialog.dart';
 import 'package:rideeye/utils/validations/validator.dart';
@@ -12,6 +15,12 @@ import 'package:shared_preferences/shared_preferences.dart';
 class LoginController extends GetxController {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
+  final TextEditingController otpOneController = TextEditingController();
+  final TextEditingController otpTwoController = TextEditingController();
+  final TextEditingController otpThreeController = TextEditingController();
+  final TextEditingController otpFourController = TextEditingController();
+  TextEditingController resetPasswordController = TextEditingController();
+  TextEditingController confirmPasswordController = TextEditingController();
   var user = {}.obs;
 
   void userLogin() async {
@@ -36,7 +45,7 @@ class LoginController extends GetxController {
           var myToken = jsondata['token'];
           prefs.setString('token', myToken);
 
-          Get.to(() => const HomePage());
+          Get.to(() => const HomePage(), transition: Transition.noTransition);
         } else {
           EasyLoading.dismiss();
           showDialog(
@@ -57,6 +66,169 @@ class LoginController extends GetxController {
           ),
         );
       }
+    }
+  }
+
+  Future<void> sendVerifcationMailForgotPassword(String email) async {
+    try {
+      EasyLoading.show(status: 'Hang on...');
+      if (emailController.text.isEmpty) {
+        EasyLoading.dismiss();
+        showDialog(
+          context: Get.context as BuildContext,
+          builder: (context) => ErrorDialog(
+            heading: 'Oops',
+            text: 'Email Field Cannot be empty',
+          ),
+        );
+      } else {
+        String body = json.encode({
+          "email": email,
+        });
+        http.Response res = await http.post(
+          Uri.parse(
+              'https://rideeyebackend.azurewebsites.net/api/forgotpassreq'),
+          headers: {'Content-Type': 'application/json'},
+          body: body,
+        );
+        var jsondata = json.decode(res.body);
+
+        if (jsondata['success'] == true) {
+          EasyLoading.dismiss();
+          Get.to(() => const EmailVerificationPW(),
+              transition: Transition.noTransition);
+        } else {
+          EasyLoading.dismiss();
+          showDialog(
+            context: Get.context as BuildContext,
+            builder: (context) => ErrorDialog(
+              heading: 'Oops',
+              text: jsondata['message'],
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      EasyLoading.dismiss();
+      showDialog(
+        context: Get.context as BuildContext,
+        builder: (context) => ErrorDialog(
+          heading: 'Oops',
+          text: 'Something Went Wrong!!',
+        ),
+      );
+    }
+  }
+
+  Future<void> verifyOTPForResetPW(String otp) async {
+    try {
+      EasyLoading.show(status: 'Verifying...');
+      String body = json.encode({
+        "email": emailController.text,
+        "otp": otp,
+      });
+
+      http.Response res = await http.post(
+        Uri.parse(
+            'https://rideeyebackend.azurewebsites.net/api/forgotpassverify'),
+        headers: {'Content-Type': 'application/json'},
+        body: body,
+      );
+      var jsondata = json.decode(res.body);
+
+      if (jsondata['success'] == true) {
+        EasyLoading.dismiss();
+        Get.to(() => const ResetPasswordScreen(),
+            transition: Transition.noTransition);
+      } else {
+        EasyLoading.dismiss();
+        showDialog(
+          context: Get.context as BuildContext,
+          builder: (context) => ErrorDialog(
+            heading: 'Oops',
+            text: jsondata['message'],
+          ),
+        );
+      }
+    } catch (e) {
+      EasyLoading.dismiss();
+      showDialog(
+        context: Get.context as BuildContext,
+        builder: (context) => ErrorDialog(
+          heading: 'Oops',
+          text: 'Something Went Wrong',
+        ),
+      );
+    }
+  }
+
+  Future<void> resetPassword() async {
+    try {
+      if (resetPasswordController.text.isEmpty ||
+          confirmPasswordController.text.isEmpty) {
+        showDialog(
+          context: Get.context as BuildContext,
+          builder: (context) => ErrorDialog(
+            heading: 'Oops',
+            text: 'Fields cannot be empty',
+          ),
+        );
+        if (resetPasswordController.text != confirmPasswordController.text) {
+          showDialog(
+            context: Get.context as BuildContext,
+            builder: (context) => ErrorDialog(
+              heading: 'Oops',
+              text: 'Password should be same',
+            ),
+          );
+          if (confirmPasswordController.text.length < 8) {
+            showDialog(
+              context: Get.context as BuildContext,
+              builder: (context) => ErrorDialog(
+                heading: 'Oops',
+                text: 'Password should be 8 character long',
+              ),
+            );
+          }
+        }
+      } else {
+        EasyLoading.show(status: 'Resetting...');
+        String body = json.encode({
+          "email": emailController.text,
+          "newPassword": confirmPasswordController.text,
+        });
+
+        http.Response res = await http.post(
+          Uri.parse('https://rideeyebackend.azurewebsites.net/api/updatepass'),
+          headers: {'Content-Type': 'application/json'},
+          body: body,
+        );
+        var jsondata = json.decode(res.body);
+
+        if (jsondata['success'] == true) {
+          EasyLoading.dismiss();
+          Get.to(() => const LoginScreen(),
+              transition: Transition.noTransition);
+        } else {
+          EasyLoading.dismiss();
+          showDialog(
+            context: Get.context as BuildContext,
+            builder: (context) => ErrorDialog(
+              heading: 'Oops',
+              text: jsondata['message'],
+            ),
+          );
+        }
+      }
+    } catch (e) {
+      EasyLoading.dismiss();
+      showDialog(
+        context: Get.context as BuildContext,
+        builder: (context) => ErrorDialog(
+          heading: 'Oops',
+          text: e.toString(),
+        ),
+      );
     }
   }
 }
