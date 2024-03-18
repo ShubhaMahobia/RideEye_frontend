46ddaac7b0bdfe68581c10d30e473b468188e531
Shubham Mahobia <mahobiashubham4@gmail.com>
2024-03-18 23:09:40 +0530

    ui completed

--- lib/userProfile/edit_profile.dart
+++ lib/userProfile/edit_profile.dart
@@ -1,10 +1,142 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:rideeye/utils/buttons/b3_user_profile.dart';
 
 class EditProfile extends StatelessWidget {
   const EditProfile({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        iconTheme: const IconThemeData(
+          color: Colors.black, //change your color here
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          'Edit Profile',
+          style: GoogleFonts.plusJakartaSans(
+              fontSize: 22, fontWeight: FontWeight.bold, color: Colors.black),
+        ),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                height: 80,
+                width: 80,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlue,
+                  borderRadius: BorderRadius.circular(100),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.065,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 60,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFFc3c3c3), width: 1.5),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.plusJakartaSans(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 60,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFFc3c3c3), width: 1.5),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.plusJakartaSans(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 60,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFFc3c3c3), width: 1.5),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.plusJakartaSans(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Container(
+                  height: 60,
+                  width: 320,
+                  decoration: BoxDecoration(
+                    border:
+                        Border.all(color: const Color(0xFFc3c3c3), width: 1.5),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: TextField(
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.plusJakartaSans(
+                          fontSize: 16, fontWeight: FontWeight.w600),
+                      decoration:
+                          const InputDecoration(border: InputBorder.none),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.165,
+              ),
+              UserProfileButton(
+                buttonText: 'SAVE',
+                onTap: () {},
+                iconPath: 'assets/images/save-instagram.png',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
